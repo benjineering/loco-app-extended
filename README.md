4a0ce63e692af9fbac76da0439c91cce2ee75b05
Ben Williams <8enwilliams@gmail.com>
2018-09-28 15:53:06 +1000

    set up as per loco cms docs

---
+++ README.md
@@ -0,0 +1,1 @@
+# LocmotiveCMS rails app
---
+++ config/initializers/dragonfly.rb
@@ -0,0 +1,22 @@
+# Configure
+Dragonfly.app(:engine).configure do
+  plugin :imagemagick,
+    convert_command:  `which convert`.strip.presence || '/usr/local/bin/convert',
+    identify_command: `which identify`.strip.presence || '/usr/local/bin/identify'
+
+  verify_urls true
+
+  secret 'df64e843e6159e0bd20855a3e39f50dd6049e387f02af10570b8d9eaef1ae98a'
+
+  url_format '/images/dynamic/:job/:sha/:basename.:ext'
+
+  fetch_file_whitelist /public/
+
+  fetch_url_whitelist /.+/
+end
+
+# Logger
+Dragonfly.logger = Rails.logger
+
+# Mount as middleware
+Rails.application.middleware.use Dragonfly::Middleware, :engine
